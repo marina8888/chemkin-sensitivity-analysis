b9bd5b4816751d3af6fc8e3f9b87694788c23276
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-06-15 15:39:02 +0900

    add graph capabilities

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from spreadsheet import create_sheet
+from spreadsheet import convert_rop_col
 
 def main():
-    hi = create_sheet.Spreadsheet('okafor_sens', 'mech.inp')
+    hi = convert_rop_col.Spreadsheet('okafor_sens', 'mech.inp')
 
 if __name__ == "__main__":
     main()
---
+++ src/spreadsheet/create_graphs.py
@@ -0,0 +1,4 @@
+import matplotlib as plt
+
+class Graph():
+    pass
