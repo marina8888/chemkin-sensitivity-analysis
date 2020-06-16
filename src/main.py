2b99902ae9fd2b79206f5c4b0af1eb8edc8d2721
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-06-16 17:51:49 +0900

    filtering values from spreadsheets

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from spreadsheet import convert_rop_col, create_graphs
 
 def main():
-    marina = create_graphs.Graph('title', 'list of eq')
-    marina.plot_bar('okafor_sens/Okafor_0.85_0.4sens.csv', 'NO', 'legend')
+    marina = create_graphs.Graph(['H+O2+H2O<=>HO2+H2O', 'H+O2<=>O+OH'], 'title')
+    marina.plot_bar_species('okafor_sens/Okafor_0.85_0.4sens.csv', 'H2O', 'legend')
     marina.show_and_save('./output/graphs', 'test')
 
 if __name__ == "__main__":
--- src/spreadsheet/create_graphs.py
+++ src/spreadsheet/create_graphs.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import numpy as np
 
 # set a global style for all graphs:
 plt.style.use('seaborn-notebook')
 
+
 class Graph():
-    def __init__(self, list_of_eq: str, title: str, x_axis_label: str = 'Sensitivity', x_graph_size: int = 6,
+    def __init__(self, list_of_eq: list, title: str, x_axis_label: str = 'Sensitivity', x_graph_size: int = 6,
                  y_graph_size: int = 6.5):
         """
         Initialise a graph, based on object arguments. One figure is created per new graph object.
@@ -23,7 +25,7 @@ class Graph():
 
         # set format variables and format the figure:
         self.x_axis_label = x_axis_label
-        self.y_axis_label = list_of_eq
+        self.list_of_x = list_of_eq
         self.title = title
 
         self.add_format()
@@ -38,18 +40,72 @@ class Graph():
         # add title, names and layout:
         plt.title(self.title, pad=15, figure=self.fig)
         plt.xlabel(self.x_axis_label, figure=self.fig)
-        plt.ylabel(self.y_axis_label, figure=self.fig)
+        self.ax.set_xticklabels(self.list_of_x, figure=self.fig)
 
-    def plot_bar(self, name_of_folder_n_sheet:str, gas_to_add:str, legend:str, multiplier:float = 1, colour:str = 'red'):
-        # convert data into df:
+    def find_col_headers(self, list_val, df, gas=None):
+        '''
+        find 'gas + equation' (from equation list) in column headers and add to list_val the true column headers:
+        '''
+        if gas is not None:
+            # call this version for species sensitivity:
+            for eq in self.list_of_x:
+                column_header = gas + '_Sens_' + eq
+                res = filter(lambda x: column_header in x, df.columns.values)
+                if res.startswith(gas):
+                    list_val.append(res)
+                    print(list_val)
+                else:
+                    list_val.append(0)
+                    print(list_val)
+        else:
+            # call this version for laminar burning velocity sensitivity:
+            for eq in self.list_of_x:
+                column_header = 'Flow_rate_Sens_' + eq
+                res = filter(lambda x: column_header in x, df.columns.values)
+
+                if res is None:
+                    list_val.append(0)
+                    print(list_val)
+                else:
+                    list_val.append(res)
+                print(list_val)
+
+        return list_val
+
+    def find_col_values(self):
+        pass
+
+    def plot_bar_species(self, name_of_folder_n_sheet: str, gas_to_add: str, legend: str, multiplier: float = 1,
+                         colour: str = 'red', X_cm: int = 2):
+        '''
+        this function takes REACTION SENSITIVITY values from a spreadsheet at a certain X(cm) distance and plots them.
+        '''
+        # convert data into df called sens_df:
         list_val = []
         full_path = os.path.join('./data/', name_of_folder_n_sheet)
         sens_df = pd.read_csv(full_path)
 
-        for eq in self.list_of_eq:
-            column_header = gas_to_add + '_Sens_' + eq
-            res = list(filter(lambda x: column_header in x, sens_df.columns.values))
-            list_val.append(tuple(res, eq))
+        # find 'gas + equation' (from equation list) in column headers and add to list_val the true column headers:
+        self.find_col_headers(list_val, sens_df, gas_to_add)
+
+        # use the column headers in list_val to return a list of values located at X(cm)
+
+        # bar chart settings:
+        bar_width = 0.25
+        ind = np.arange(len(list_val))
+
+        self.ax.barh(ind, list_val, bar_width, align='center')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_xticks(ind + bar_width / 2)
+        self.ax.legend()
+
+    def plot_bar_lam_burning_v(self, name_of_folder_n_sheet: str, legend: str, multiplier: float = 1,
+                               colour: str = 'green', X_cm: int = 0):
+        '''
+        this function takes LAMINAR BURNING VELOCITY SENSITIVITY and plots it on a bar chart at default distance = 0 cm.
+        '''
+        pass
 
     def show_and_save(self, path_of_save_folder: str, name: str):
         """
@@ -60,5 +116,5 @@ class Graph():
         :return:
         """
         full_path = os.path.join(path_of_save_folder, name)
-        plt.savefig(full_path, dpi=300, bbox_inches = "tight")
+        plt.savefig(full_path, dpi=300, bbox_inches="tight")
         plt.close(self.fig)
