198bb0be6a6cc326fe0d5eea8d24b87d9036df07
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-06-23 11:34:15 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -10,8 +10,8 @@ def main():
     marina.plot_bar_lam_burning_v('data/okafor_lam_sens/export.csv', ['NH2+O<=>NH+OH', 'NH2+NO<=>NNH+OH','H+NO+M<=>HNO+M', 'HNO+H<=>H2+NO', 'H+O2<=>O+OH'])
 
     #Add species plots for NO and NO2, with an offset create grouped bars, X distance defining chamber end and multiplier:
-    # marina.plot_bar_species('data/okafor_sens/Okafor_0.85_0.4sens.csv', 'NO', ['NH2+O<=>NH+OH', 'NH2+NO<=>NNH+OH','H+NO+M<=>HNO+M', 'HNO+H<=>H2+NO', 'H+O2<=>O+OH'], offset=0.15)
-    # marina.plot_bar_species('data/okafor_sens/Okafor_0.85_0.4sens.csv', 'NO2', ['NH2+O<=>NH+OH', 'NH2+NO<=>NNH+OH','H+NO+M<=>HNO+M', 'HNO+H<=>H2+NO', 'H+O2<=>O+OH'], multiplier = 10, X=0.02, colour = 'g', offset=0.3)
+    marina.plot_bar_species('data/okafor_sens/Okafor_0.85_0.4sens.csv', 'NO', ['NH2+O<=>NH+OH', 'NH2+NO<=>NNH+OH','H+NO+M<=>HNO+M', 'HNO+H<=>H2+NO', 'H+O2<=>O+OH'], offset=0.15)
+    marina.plot_bar_species('data/okafor_sens/Okafor_0.85_0.4sens.csv', 'NO2', ['NH2+O<=>NH+OH', 'NH2+NO<=>NNH+OH','H+NO+M<=>HNO+M', 'HNO+H<=>H2+NO', 'H+O2<=>O+OH'], multiplier = 10, X=0.02, colour = 'g', offset=0.3)
 
     #Save graphs to ./output/graphs folder (which user may need to create) under the name test.png:
     marina.show_and_save('./output/graphs', 'test')
